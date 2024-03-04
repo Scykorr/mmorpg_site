1b4dbfdf0ede7e8f91abf5686eefd095f5ba91f4
Tony <fedosov-toxa@mail.ru>
2024-03-05 00:49:58 +0300

    add some models

--- mmorpg_fan_site/fun_site/models.py
+++ mmorpg_fan_site/fun_site/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    tank = 'TNK'
+    heal = 'HL'
+    dd = 'DD'
+    merchant = 'MRCHNT'
+    guildmaster = 'GLDMSTR'
+    questgiver = 'QSTGVR'
+    blacksmith = 'BLCKSMTH'
+    tanner = 'TNNR'
+    potionmaster = 'PTNMSTR'
+    wizard = 'WZRD'
+
+    TYPES = [
+        ('TNK', 'Танк'),
+        ('HL', 'Хил'),
+        ('DD', 'ДД'),
+        ('MRCHNT', 'Торговец'),
+        ('GLDMSTR', 'Гильдмастер'),
+        ('QSTGVR', 'Квестгивер'),
+        ('BLCKSMTH', 'Кузнец'),
+        ('TNNR', 'Кожевник'),
+        ('PTNMSTR', 'Зельевар'),
+        ('WZRD', 'Мастер заклинаний')
+
+    ]
+    category = models.CharField(max_length=8, choices=TYPES, default=dd)
+
+class Person(models.Model):
+    pass
+class Post(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
--- mmorpg_fan_site/mmorpg_fan_site/settings.py
+++ mmorpg_fan_site/mmorpg_fan_site/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.flatpages',
     'fpages',
+    'fun_site',
 ]
 
 SITE_ID = 1
