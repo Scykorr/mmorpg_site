e4d6e632d5409c047c0d259c4ca6a6a3519135ef
Tony <fedosov-toxa@mail.ru>
2024-03-11 23:01:30 +0300

    done

--- mmorpg_fan_site/accounts/urls.py
+++ mmorpg_fan_site/accounts/urls.py
@@ -4,7 +4,4 @@ from .views import AccountProfile, UpdateProfile, auth_code
 
 urlpatterns = [
     path('', include('allauth.urls')),
-    path('profile/', AccountProfile.as_view(), name='account_profile'),
-    path('account_edit/', UpdateProfile.as_view(), name='account_edit'),
-    path('auth_code/', auth_code, name='auth_code')
 ]
--- mmorpg_fan_site/fun_site/tasks.py
+++ mmorpg_fan_site/fun_site/tasks.py
@@ -1,51 +1,26 @@
 from celery import shared_task
-from django.contrib.auth.models import User
-from .models import Post, Comment
+from .models import Comment
 from django.core.mail import send_mail
-from datetime import timedelta
-from django.utils import timezone
 
 
 @shared_task
-def respond_send_email(respond_id):
-    respond = Response.objects.get(id=respond_id)
+def comment_send_email(comment_id):
+    comment = Comment.objects.get(id=comment_id)
     send_mail(
-        subject=f'MMORPG Billboard: новый отклик на объявление!',
-        message=f'Доброго дня, {respond.post.author}, ! На Ваше объявление есть новый отклик!\n'
-                f'Прочитать отклик:\nhttp://127.0.0.1:8000/responses/{respond.post.id}',
-        from_email='nogerovarthur24@yandex.com',
-        recipient_list=[respond.post.author.email],
+        subject=f'best MMORPG: новый комментарий на пост!',
+        message=f'Доброго дня, {comment.post.person.username}, ! На Ваш пост есть новый комментарий\n',
+         from_email='fedos.py@ya.ru',
+        recipient_list=[comment.post.person.email],
     )
 
 
 @shared_task
-def respond_accept_send_email(comment_id):
-    respond = Comment.objects.get(id=comment_id)
-    print(respond.post.author.email)
+def comment_accept_send_email(comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    print(comment.post.author.email)
     send_mail(
-        subject=f'MMORPG Billboard: Ваш отклик принят!',
-        message=f'Доброго дня, {respond.author}, Автор объявления {respond.post.title} принял Ваш отклик!\n'
-                f'Посмотреть принятые отклики:\nhttp://127.0.0.1:8000/responses',
-        from_email='nogerovarthur24@yandex.com',
-        recipient_list=[respond.post.author.email],
+        subject=f'best MMORPG: Ваш комментарий принят!',
+        message=f'Доброго дня, {comment.person}, Автор поста {comment.post.title} принял Ваш комментарий!\n',
+        from_email='fedos.py@ya.ru',
+        recipient_list=[comment.post.person.email],
     )
-
-
-@shared_task
-def send_mail_monday_8am():
-    now = timezone.now()
-    list_week_posts = list(Post.objects.filter(
-        dateCreation__gte=now - timedelta(days=7)))
-    if list_week_posts:
-        for user in User.objects.filter():
-            print(user)
-            list_posts = ''
-            for post in list_week_posts:
-                list_posts += f'\n{post.title}\nhttp://127.0.0.1:8000/post/{post.id}'
-            send_mail(
-                subject=f'News Portal: посты за прошедшую неделю.',
-                message=f'Доброго дня, {user.username}!\nПредлагаем Вам ознакомиться с новыми объявлениями, '
-                        f'появившимися за последние 7 дней:\n{list_posts}',
-                from_email='nogerovarthur24@yandex.com',
-                recipient_list=[user.email],
-            )
